9ea4ddebc811171bf8338017d157c79f8c31f289
Seijitsu <seijitsu_75@hotmail.fr>
2013-11-20 21:28:01 +0100

    adm
    Pas de réel modif j'ai juste fait des retours à la ligne pour le tableau

--- administration/adm.php
+++ administration/adm.php
@@ -4,6 +4,8 @@
         <title> Administration JPO </title>
         <meta charset="utf-8">
         <link href="../css/bootstrap.css" rel="stylesheet">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
     </head>
 
     <body>
@@ -39,7 +41,79 @@
             echo '<tr class="success"><td>Nom</td> <td>Prénom</td> <td>Date de naissance</td> <td>Ville</td> <td>Sexe</td> <td>Mail</td> <td>Etablissement d\'origine</td> <td>Bac</td> <td>Autre bac</td> <td>Filière antérieure</td> <td>Option du bac</td> <td>Expérience professionnelle</td> <td>Connaissances en info</td> <td>Avis sur la JPO</td> <td>Connaissance filère</td> <td>Autre connaissance filère</td> <td>Temps de trajet</td> <td>Motivation</td> </tr>';
             while ($donnees = $reponse->fetch()) {
 
-                echo '<tr><td>' . $donnees['NOM'] . '</td><td>' . $donnees['PRENOM'] . '</td><td>' . $donnees['DATENAISSANCE'] . '</td><td>' . $donnees['VILLE'] . '</td><td>' . $donnees['SEXE'] . '</td><td>' . $donnees['ADRMAIL'] . '</td><td>' . $donnees['ETABLISSEMENT'] . '</td><td>' . $donnees['BAC'] . '</td><td>' . $donnees['BACAUTRE'] . '</td><td>' . $donnees['FILIERE'] . '</td><td>' . $donnees['OPTIONS'] . '</td><td>' . $donnees['EXPPRO'] . '</td><td>' . $donnees['CONNAISSANCEINF'] . '</td><td>' . $donnees['AVIS'] . '</td><td>' . $donnees['CONNAISSANCESIO'] . '</td><td>' . $donnees['CONNAISSANCEAUTRE'] . '</td><td>' . $donnees['TPSTRAJET'] . '</td><td>' . $donnees['MOTIVATION'] . '</td></tr>';
+                echo '<tr>
+                        <td id="test" idnom='.$donnees['NUMID'].'>' 
+                            . $donnees['NOM'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['PRENOM'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['DATENAISSANCE'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['VILLE'] . '
+                        </td>
+
+                        <td>'
+                            . $donnees['SEXE'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['ADRMAIL'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['ETABLISSEMENT'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['BAC'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['BACAUTRE'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['FILIERE'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['OPTIONS'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['EXPPRO'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['CONNAISSANCEINF'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['AVIS'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['CONNAISSANCESIO'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['CONNAISSANCEAUTRE'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['TPSTRAJET'] . '
+                        </td>
+
+                        <td>' 
+                            . $donnees['MOTIVATION'] . '
+                        </td>
+                    </tr>';
             }
 
             echo '</table>';
@@ -47,11 +121,6 @@
             $reponse->closeCursor();
         }
 
-
-
-
-
-//echo $reponse;
         ?>
     </body>
 
